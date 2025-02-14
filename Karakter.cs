2bcdded7be0154c0ffd320043d587d9548748cec
xhBIROhx <birorihard@gmail.com>
2025-02-14 11:46:19 +0100

    órai init

---
+++ Karakter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orai
+{
+	internal class Karakter
+	{
+		private string name;
+		private int szint;
+		private int eletero;
+		private int ero;
+
+		public Karakter(string name, int szint, int eletero, int ero)
+		{
+			this.name = name;
+			this.szint = szint;
+			this.eletero = eletero;
+			this.ero = ero;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Szint { get => szint; set => szint = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Ero { get => ero; set => ero = value; }
+
+		public override string? ToString()
+		{
+			return $"{name} - {szint} / {eletero} / {ero}";
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace orai
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Karakter> karakterek = [];
+
+			Beolvasas("karakterek.txt", karakterek);
+
+			foreach (var item in karakterek)
+			{
+				Console.WriteLine(item.ToString());
+			}
+		}
+		static void Beolvasas(string filenev, List<Karakter> karakterek)
+		{
+			StreamReader sr = new(filenev);
+
+            sr.ReadLine();
+
+			while (!sr.EndOfStream) 
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(';');
+
+				Karakter karakter = new Karakter(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
+        }
+
+	}
+}
